7ce30d9f335798e5b0f326bcd6d4acb7c33d9394
Aaboassoud <a.aboassoud@gmail.com>
2022-06-29 21:40:02 +0300

    updated

--- js.js
+++ js.js
@@ -1,37 +1,41 @@
 let words = ["the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","solo","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part"];
-
+let seconds = 30
+let randomWords=[]
+let countInput = []
+let time = document.getElementById("timer")
+let input = document.getElementById("input1")
+let startBtn = document.getElementById("start")
+let wordsPerSecond=document.getElementById("wps")
+let para = document.getElementById("text-paragraph")
 print = console.log
-print(words.length)
-
-
-let randomWords=[];
 random = Math.ceil(Math.random()*99)
 
-var seconds = 30;                 
-time = document.getElementById("timer")
-let wordsPerSecond=document.getElementById("wps")
+
 function myLoop() {         
   setTimeout(function() {   
-    console.log(seconds); 
-    time.innerHTML = seconds 
-    seconds--;                    
-    if (seconds >= 0) {           
-      myLoop();            
+    console.log(seconds);
+    time.textContent = `${seconds}s`
+    seconds--;
+    if (seconds >= 0) {
+      myLoop();
     } 
     if (seconds < 0 ){
-        document.getElementById("text-paragraph").style.display="none"
-        document.getElementById("input1").style.display = "none"
+        para.style.display ="none"
+        input.className = "d-none"
+        time.textContent = "Finish"
+        startBtn.textContent = "Reload"
+        startBtn
+        startBtn.addEventListener("click", ()=>{
+            window.location.reload()
+        })
         let word = countInput.length
-        let wps = word/30
-        wordsPerSecond.innerHTML= `${wps} Word per Second\n(wps)\n`
+        let wps = (word*2)
+        wordsPerSecond.innerHTML= `"${Math.ceil(wps)}" Word per Min\n(wpm)\n`
     }
   }, 1000)
 }
 
 
-
-countInput = []
-para = document.getElementById("text-paragraph")
 document.getElementById("start").addEventListener("click",()=>{
     myLoop();
     randomWords=[];
@@ -41,11 +45,12 @@ document.getElementById("start").addEventListener("click",()=>{
         random = Math.ceil(Math.random()*99)
     } print(randomWords)
     para.innerHTML = randomWords.join(' ')
-    
+    input.className = "d-inline"
+    input.focus()
     print(para.textContent)
 })
 
-input = document.getElementById("input1")
+
 input.addEventListener("keydown",(e)=>{
     if (e.keyCode === 32){
         if (input.value == randomWords[0]){
